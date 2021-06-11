14d26c1281f3d19af35f9ff73bbb411ccdfad3e8
ParkSeungSu <cldkr0401@naver.com>
2021-06-11 13:31:41 +0900

    start

---
+++ djangoStudy/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/parkseungsu/Documents/GitHub/DjangoStudy/djangoStudy/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
