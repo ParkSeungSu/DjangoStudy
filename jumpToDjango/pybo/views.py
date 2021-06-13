3d7a468e6496046a149aed870d80d40b7d8860a8
ParkSeungSu <cldkr0401@naver.com>
2021-06-13 16:04:54 +0900

    3-03
    paging

--- jumpToDjango/pybo/views.py
+++ jumpToDjango/pybo/views.py
@@ -1,7 +1,10 @@
+from django.core import paginator
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Question
 from django.utils import timezone
+from .forms import AnswerForm, QuestionForm
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -9,8 +12,18 @@ def index(request):
     '''
     pybo 목록 출력
     '''
+    #입력 파라메터
+    page = request.GET.get('page','1')
+
+    #조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    #페이징 처리
+    paginator = Paginator(question_list, 10)#페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+
+    context = {'question_list': page_obj}
 
     return render(request,'pybo/question_list.html',context)
 
@@ -27,5 +40,31 @@ def answer_create(request, question_id):
     pybo 답변 등록
     '''
     question = get_object_or_404(Question,pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
-    return redirect('pybo:detail',question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer=form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail',question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question':question, 'form':form}
+    return render(request,'pybo/question_detail.html',context)
+
+def question_create(request):
+    '''
+    pybo 질문 등록
+    '''
+    if request.method=='POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request,'pybo/question_form.html',context)
