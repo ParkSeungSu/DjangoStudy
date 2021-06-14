d6d5590ed127e1ecc8dc741bc8c579cf3ba6a436
ParkSeungSu <cldkr0401@naver.com>
2021-06-14 16:45:05 +0900

    3-07
    require Login
    When unauthorized user write Questions and Answers

--- jumpToDjango/pybo/views.py
+++ jumpToDjango/pybo/views.py
@@ -5,6 +5,7 @@ from .models import Question
 from django.utils import timezone
 from .forms import AnswerForm, QuestionForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -35,6 +36,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request,'pybo/question_detail.html',context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     '''
     pybo 답변 등록
@@ -44,6 +46,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer=form.save(commit=False)
+            answer.author=request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -52,7 +55,8 @@ def answer_create(request, question_id):
         form = AnswerForm()
     context = {'question':question, 'form':form}
     return render(request,'pybo/question_detail.html',context)
-
+    
+@login_required(login_url='common:login')
 def question_create(request):
     '''
     pybo 질문 등록
@@ -61,6 +65,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
