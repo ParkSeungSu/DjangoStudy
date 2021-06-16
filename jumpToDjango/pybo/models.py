9f8887dead4cbaa2023ea37d4ea5e9e64bfdb230
ParkSeungSu <cldkr0401@naver.com>
2021-06-16 16:43:55 +0900

    3-13
    추천기능 추가

--- jumpToDjango/pybo/models.py
+++ jumpToDjango/pybo/models.py
@@ -6,8 +6,9 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
-    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True,related_name='author_question')
     modify_date = models.DateTimeField(null=True,blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):
         return self.subject
@@ -18,9 +19,10 @@ class Answer(models.Model):
     create_date = models.DateTimeField()
     author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     modify_date = models.DateTimeField(null=True,blank=True)
+    voter = models.ManyToManyField(User,related_name='voter_answer')
 
 class Comment(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='author_name')
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True,blank=True)
