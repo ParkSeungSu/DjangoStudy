1681b9abe8a63cfa8d69c152b840709ff5e85309
ParkSeungSu <cldkr0401@naver.com>
2021-06-18 12:37:08 +0900

    MVC
    Model - models.py
    View - templates
    Control - views.py

--- secondWeb/elections/models.py
+++ secondWeb/elections/models.py
@@ -5,7 +5,9 @@ from django.db.models.base import Model
 
 class Candidate(models.Model):
     name = models.CharField(max_length=10)
-    introdution = models.TextField()
+    introduction = models.TextField()
     area = models.CharField(max_length=15)
-    party_number = models.IntegerField(default=1)
+    party_number = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
--- secondWeb/elections/views.py
+++ secondWeb/elections/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Candidate
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    candidates = Candidate.objects.all()
+    context = {'candidates' : candidates}
+    return render(request,'elections/index.html',context)
 
