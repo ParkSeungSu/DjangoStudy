25c6a90dcd366d0e1bc1cc71cf83b63e5939ebb4
ParkSeungSu <cldkr0401@naver.com>
2021-06-20 13:04:55 +0900

    Fail..

--- django_react/leads/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Lead
-
-class LeadSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lead
-        exclude = ('id','name','email','message')
-        depth = 4
